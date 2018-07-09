c519547e6d7eb26bc511d38bdb6aa82f8c11db37
Odelia <odeliasusanto@gmail.com>
2018-07-09 18:39:23 +0700

    added dynamic coding

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 SET openingDate with 7
 STORE `endDate` with 31
 STORE `names` with {2: 'Tono', 4: 'Anton', 5: Budi'}
-STORE `result` = 'Tanggal` + i + ': ' + arr
 
 FOR i = 7 to `endDate`
 STORE `date` with value minus 7
@@ -21,13 +20,9 @@ END IF
 DISPLAY `result`
 */
 
-function fitnessCalendar() {
+function fitnessCalendar(names) {
     let endDate = 31;
-    let names = {
-        2: 'Tono',
-        4: 'Anton',
-        5: 'Budi'
-    }
+    
     
     for (let i = 7; i <= endDate; i++) {
         let date = i - 7;
@@ -46,5 +41,11 @@ function fitnessCalendar() {
     }
 }
 
-fitnessCalendar();
+let names = {
+    2: 'Tono',
+    4: 'Anton',
+    5: 'Budi',
+}
+
+fitnessCalendar(names);
 
