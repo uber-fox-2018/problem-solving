0077434bf1c35d15c302b7722a6f24ea54750d0c
Khodhi Robbani <khodhirobbani@gmail.com>
2018-07-09 18:19:48 +0700

    revisi koma dengan split dan join

--- ProblemSolvers.js
+++ ProblemSolvers.js
@@ -10,11 +10,7 @@
   
   FOR i equal to 7 less then equal to 31
     IF i equal to 7
-      FOR j equal to 0 less then length of param index 0
-        IF j equal to length of param index 1 minus 1
-          STORE nama with param[0][j]
-        ELSE 
-          STORE nama with param[0][j] ,
+      FOR j equal to 0 less then length of param index 0                   STORE nama with param[0][j] ,
         add j by 1
 
     FOR j less then param[1][j] 
@@ -25,6 +21,12 @@
     IF i modulus 5 equal to 0
       STORE nama with tempat fitness Tutup
 
+    STORE split equal to nama.split('')
+       FOR l less then split length
+         IF split index split.length minus 2 equal to ', '
+           split index split.length minus 2 equal to empty string
+    STORE nama equal to split.join
+    
     print tanggal i nama
     nama equal to empty string
 */
@@ -40,12 +42,9 @@ function namaJadwal (param){
       for (var i=7 ; i <= 31 ; i++){
         if ( i == 7){
           for (var j=0 ; j <param[0].length ; j++){
-            if (j == param[1].length -1){
-              nama += param[0][j]
-            }
-            else {
+            
             nama += param[0][j] + ', '
-            }
+            
           }
         }
        
@@ -56,8 +55,16 @@ function namaJadwal (param){
           }
         }
          if (i%5 ==0 ){
-          nama = 'Tempat Fitness Tutup'
+          nama = 'Tempat Fitness Tutup, '
         }
+
+      var split = nama.split('')
+      for (var l=0 ; l < split.length ; l++){
+        if (split[split.length-2] == ','){
+          split[split.length-2] = ''
+        }
+      }
+      var nama = split.join('')
        console.log('Tanggal ' + i +': ' + nama)
         nama = ''
       }
