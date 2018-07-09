3324d8052082cf33bac35c1bfde1c28f7ace262a
Susan Nio <susan.wen88@gmail.com>
2018-07-09 17:09:45 +0700

    Updating solution to become dynamic

--- problemSolving.js
+++ problemSolving.js
@@ -1,84 +1,77 @@
 /* PSEUDOCODE
-FUNCTION problemSolving with parameter num:number
-  STORE "user1" with "Tono"
-  STORE "user2" with "Anton"
-  STORE "user3" with "Budi"
-  STORE "tono" with empty array
-  STORE "anton" with empty array
-  STORE "budi" with empty array
-  STORE "i" with 7
-  FOR "i" to num 
-    PUSH "i" to "tono"
-    SET "i" with "i" + 1
-  END FOR
-  STORE "j" with 7
-  FOR "j" to num 
-    PUSH "j" to "anton"
-    SET "j" with "j" + 4
-  END FOR
-  STORE "k" with 7
-  FOR "k" to num 
-    PUSH "k" to "budi"
-    SET "k" with "k" + 5
-  END FOR
-  STORE "l" with 7
-  FOR "l" to num
-    IF "l" MOD 5 EQUALS 0 THEN 
-      DISPLAY "Tanggal " + l + ": Tempat Fitness Tutup"
-    ELSE IF "l" MOD 2 EQUALS 1 THEN 
-        IF "anton" INCLUDES l AND "tono" INCLUDES l AND "budi" INCLUDES l THEN
-            DISPLAY "Tanggal " + l + ": " + "user1" + ", " + "user2" + ", " + "user3"
-        ELSE IF "anton" INCLUDES l AND "tono" INCLUDES l THEN 
-            DISPLAY "Tanggal " + l + ": " + "user1" + ", " + "user2"
-        ELSE IF "budi" INCLUDES l AND "tono" INCLUDES l THEN 
-            DISPLAY "Tanggal " + l + ": " + "user1" + ", " + "user3" 
-        ELSE IF "budi" NOT INCLUDES l AND "tono" INCLUDES l OR "anton" NOT INCLUDES l AND "tono" INCLUDES l THEN 
-            DISPLAY "Tanggal " + l + ": " + "user1"
+FUNCTION problemSolving with parameter arr:array
+    STORE "days" with empty array
+    STORE "users" with empty array
+    STORE "mod" with empty array
+    STORE "user" with empty array
+    STORE "arrFinal" with empty array
+    STORE "i" with 0
+    FOR "i" to length of arr 
+        PUSH arr[i][0] to "users"
+        PUSH arr[i][1] to "days"
+        PUSH CALCULATE 7 % arr[i][1] to "mod"
+        SET "i" with "i" + 1
+    END FOR
+    STORE "j" with 7
+    FOR "j" to 31
+        IF "j" MOD 5 EQUALS 0 THEN 
+            PUSH [j,["Tempat Fitness Tutup"]] to "arrFinal"
+        ELSE 
+            STORE "k" with 0
+            FOR "k" to length of "mod"
+                IF "j" MOD days[k] EQUALS mod[k] THEN 
+                    PUSH users[k] to "user"
+                END IF
+                SET "k" with "k" + 1
+            END FOR
+            PUSH [j, user] to "arrFinal"
+            SET "user" with empty array
+        END IF 
+        SET "j" with "j" + 1
+    END FOR
+    STORE "l" with 0
+    FOR "l" to length of arrFinal
+        IF length of arrFinal[l][1] EQUALS 0 THEN
+          DISPLAY "Tanggal " + arrFinal[l][0] + ": "
+        ELSE 
+          DISPLAY "Tanggal " + arrFinal[l][0] + ": " + arrFinal[l][1]
         END IF
-    ELSE IF "budi" INCLUDES l THEN
-        DISPLAY "Tanggal " + l + ": " + "user3"
-    ELSE 
-        DISPLAY "Tanggal " + l + ": "
-    END IF
-    SET "l" with "l" + 1
-  END FOR
+        SET "l" with "l" + 1
+    END FOR
 END FUNCTION
  */
-function problemSolving(num){
-    let user1= "Tono";
-    let user2= "Anton";
-    let user3= "Budi";
-    let tono=[];
-    let anton = [];
-    let budi = [];
-    for(let i=7;i<=num;i++){
-        tono.push(i);
-    }
-    for(let j=7;j<=num;j+=4){
-        anton.push(j);
-    }
-    for(let k=7;k<=num;k+=5){
-        budi.push(k);
+
+function problemSolving(arr){
+    let days=[];
+    let users=[];
+    let mod=[];
+    let user=[];
+    let arrFinal=[];
+    for(let i=0 ; i<arr.length;i++){
+        users.push(arr[i][0]);
+        days.push(arr[i][1]);
+        mod.push(7%arr[i][1]);
     }
 
-    for(let l=7;l<=num;l++){
-        if(l%5===0){
-            console.log(`Tanggal ${l}: Tempat Fitness Tutup`);
-        }else if(l%2===1){
-            if(anton.includes(l) && tono.includes(l) && budi.includes(l)){
-                console.log(`Tanggal ${l}: ${user1}, ${user2}, ${user3} `);
-            }else if(anton.includes(l) && tono.includes(l)){
-                console.log(`Tanggal ${l}: ${user1}, ${user2} `);
-            }else if(budi.includes(l)&& tono.includes(l)){
-                console.log(`Tanggal ${l}: ${user1}, ${user3} `);
-            }else if(!budi.includes(l) && tono.includes(l) || !anton.includes(l) && tono.includes(l)){
-                console.log(`Tanggal ${l}: ${user1}`);
-            }
-        }else if(budi.includes(l)){
-            console.log(`Tanggal ${l}: ${user3}`);
+    for(let j=7;j<=31;j++){
+        if(j%5 === 0){
+           arrFinal.push([j,["Tempat Fitness Tutup"]]);
         }else{
-            console.log(`Tanggal ${l}:`);
+            for(let k=0;k<mod.length;k++){
+                if (j%days[k]===mod[k]){
+                   user.push(users[k]);
+                }       
+            }
+            arrFinal.push([j, user]);
+            user=[];
         }
     }
+   for(let l=0;l<arrFinal.length;l++){
+    if(arrFinal[l][1].length===0){
+      console.log("Tanggal " + arrFinal[l][0] + ": ")
+    }else{
+      console.log("Tanggal " + arrFinal[l][0] + ": " + arrFinal[l][1]);
+    }
+   }    
 }
-problemSolving(31);
+console.log(problemSolving([["Tono", 2], ["Anton", 4], ["Budi", 5]]));
